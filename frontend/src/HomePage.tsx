5fed7b29560e3379e145e5301f2f83dbc2784be8
Jose Miralles <josemiralles1991@gmail.com>
2021-04-07 01:31:09 -0400

    Removed git submodule, and started working on questions on the front end.

---
+++ frontend/src/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { QuestionList } from "./QuestionList";
+import { getUnansweredQuestions } from "./QuestionsData";
+
+export const HomePage = () => (
+    <div>
+        <div>
+            <h2>Unanswered Questions</h2>
+            <button>Ask a question</button>
+            <QuestionList data={ getUnansweredQuestions() } />
+        </div>
+    </div>
+);
