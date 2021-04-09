9374ffb12c2ff45d484dbf12abdb20294fdba75b
Jose Miralles <josemiralles1991@gmail.com>
2021-04-09 16:10:59 -0400

    implemented form validation in the front end for the question form

--- frontend/src/Pages/AskPage.tsx
+++ frontend/src/Pages/AskPage.tsx
@@ -1,9 +1,48 @@
 import React from "react";
 import { Page } from "../Page";
+import { useForm } from "react-hook-form";
 
-const AskPage = () => {
+import "../styles/form.scss";
+
+type FormData = {
+    title: string;
+    content: string;
+};
+
+const AskPage = (): JSX.Element => {
+
+    const { register, watch, formState: { errors } } = useForm<FormData>({ mode: "onBlur" });
+
     return (
-        <Page title="Ask a question">{ null }</Page>
+        <Page title="Ask a question">
+            <form className="form card-shadow">
+
+
+                {errors.title && errors.title.type === "required" && (
+                    <b className="error text-danger">You must enter the question title!</b>
+                )}
+                {errors.title && errors.title.type === 'minLength' && (
+                    <b className="error text-danger">Title needs to be at least 10 characters!</b>
+                )}
+                <label htmlFor="title">Title</label>
+                <input id="title" type="text"
+                    {...register("title", { required: true, minLength: 10 })} />
+
+                {errors.content && errors.content.type === "required" && (
+                    <b className="error text-danger">You must enter the question content!</b>
+                )}
+                {errors.content && errors.content.type === "minLength" && (
+                    <b className="error text-danger">The body needs to be at least 50 characters!</b>
+                )}
+                <label htmlFor="content">Content</label>
+                <textarea id="content"
+                    {...register("content", {required: true, minLength: 50})}
+                />
+
+                <button className="btn btn-primary" type="submit">Submit Question</button>
+
+            </form>
+        </Page>
     );
 };
 
--- frontend/src/Pages/QuestionPage.tsx
+++ frontend/src/Pages/QuestionPage.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 import { Page } from "../Page";
 import { QuestionData, getQuestion } from "../QuestionsData";
 import { AnswerList } from "../AnswerList";
 
 import "../styles/Question.scss";
+import "../styles/form.scss";
 
 export const QuestionPage = () => {
 
     const { questionId } = useParams();
-
     const [question, setQuestion] = React.useState<QuestionData | null>();
+    const { register, watch, errors } = useForm<FormData>({mode: 'onBlur'});
     
     useEffect(() => {
 
@@ -44,7 +46,16 @@ export const QuestionPage = () => {
 
                         <hr />
                         
-                        {question.answers && <AnswerList data={ question.answers } /> }
+                        {question.answers && <AnswerList data={question.answers} />}
+                        
+                        <form className="form shadow">
+                            <label htmlFor="content">Your Answer</label>
+                            <textarea id="content"
+                                {...register("content", { required: true, minLength: 10 })}
+                            />
+                            
+                            <button className="btn btn-primary" type="submit">Submit Your Answer</button>
+                        </form>
                     </>
                 )}
 
